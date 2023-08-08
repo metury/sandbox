e31f1363d8f977e1842f614dc2fe0d7b7e401ebd
Tomáš Turek <tom.turek@atlas.cz>
2023-08-08 23:08:01 +0200

    prvni vysledky

---
+++ 2048/README.md
@@ -0,0 +1,10 @@
+# 2048
+
+Simulace hry 2048 a testování náhodných jednoduchých solverů.
+
+- 0 je random
+- 1 je best
+- 2 je leftup
+- 3 je cycle
+
+![plot](plot.png)
--- 2048/tests.sh
+++ 2048/tests.sh
@@ -2,30 +2,41 @@
 
 set -ueo pipefail
 
+if [ $# -lt 1 ]; then
+	echo "One argument must be present: number of tests."
+	exit
+fi
+
 dir="tests"
 results="RESULTS"
 
 mkdir -p $dir
 touch $results
 
-counter=$((30))
+counter=$1
 
 while [[ $counter != 0 ]]; do
 	counter=$(( $counter - 1 ))
+	
 	file=$dir/"random"$counter
 	go run 2048.go -r > $file
 	printf "0 " >> $results
 	tail -n 1 $file >> $results
-	file=$dir/"random"$counter
-	go run 2048.go -b > $dir/"best"$counter
+	
+	file=$dir/"best"$counter
+	go run 2048.go -b > $file
 	printf "1 " >> $results
 	tail -n 1 $file >> $results
-	file=$dir/"random"$counter
-	go run 2048.go -lu > $dir/"lu"$counter
+	
+	file=$dir/"lu"$counter
+	go run 2048.go -lu > $file
 	printf "2 " >> $results
 	tail -n 1 $file >> $results
-	file=$dir/"random"$counter
-	go run 2048.go -c > $dir/"cycle"$counter
+	
+	file=$dir/"cycle"$counter
+	go run 2048.go -c > $file
 	printf "3 " >> $results
 	tail -n 1 $file >> $results
 done
+
+./plot.py
