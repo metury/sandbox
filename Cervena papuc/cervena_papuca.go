c88d02afb16fbe4095d317c60abd0688fef1ff33
Tomáš Turek <tom.turek@atlas.cz>
2024-11-30 20:35:27 +0100

    removed obsolete files

--- Cervena papuc/cervena_papuca.go
+++ Cervena papuc/cervena_papuca.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-func genPacks(packSize int) []int{
+func genPacks(packSize int) []int {
 	pack := rand.Perm(packSize)
 	for i := 0; i < packSize; i++ {
 		pack[i] = (pack[i] % 2)
@@ -15,66 +15,66 @@ func genPacks(packSize int) []int{
 	return pack
 }
 
-func genNormalPack() []int{
-	pack := []int{0,0,0,0,0,0,1,1,
-		0,0,0,0,0,0,1,1,
-		0,0,0,0,0,0,1,1,
-		0,0,0,0,0,0,1,1}
+func genNormalPack() []int {
+	pack := []int{0, 0, 0, 0, 0, 0, 1, 1,
+		0, 0, 0, 0, 0, 0, 1, 1,
+		0, 0, 0, 0, 0, 0, 1, 1,
+		0, 0, 0, 0, 0, 0, 1, 1}
 	rand.Shuffle(len(pack), func(i, j int) {
 		pack[i], pack[j] = pack[j], pack[i]
 	})
 	return pack
 }
 
-func printCard(card int){
-	if card == 1{
+func printCard(card int) {
+	if card == 1 {
 		fmt.Print("♦️")
 	} else {
 		fmt.Print("♠️")
 	}
 }
 
-func printStack(stack *[]int){
+func printStack(stack *[]int) {
 	fmt.Print("Kolo: ")
-	for i := 0; i < len(*stack); i++{
+	for i := 0; i < len(*stack); i++ {
 		printCard((*stack)[i])
 	}
 	fmt.Println()
 }
 
-func singleStep(playingFirst bool, firstPack *[]int, secondPack *[]int) bool{
+func singleStep(playingFirst bool, firstPack *[]int, secondPack *[]int) bool {
 	var stack []int
 	var current int
 	for current != 1 {
-		if playingFirst && len(*firstPack) > 0{
+		if playingFirst && len(*firstPack) > 0 {
 			current = (*firstPack)[0]
 			(*firstPack) = (*firstPack)[1:]
 		} else if len(*secondPack) > 0 {
 			current = (*secondPack)[0]
 			(*secondPack) = (*secondPack)[1:]
-		} else{
+		} else {
 			break
 		}
 		stack = append(stack, current)
 		playingFirst = !playingFirst
 	}
 	printStack(&stack)
-	if !playingFirst{
+	if !playingFirst {
 		(*secondPack) = append((*secondPack), stack[:]...)
-	} else{
+	} else {
 		(*firstPack) = append((*firstPack), stack[:]...)
 	}
 	return playingFirst
 }
 
-func printPacks(firstPack *[]int, secondPack *[]int){
+func printPacks(firstPack *[]int, secondPack *[]int) {
 	fmt.Print("První balíček: ")
-	for i := 0; i < len((*firstPack)); i++{
+	for i := 0; i < len((*firstPack)); i++ {
 		printCard((*firstPack)[i])
 	}
 	fmt.Println()
 	fmt.Print("Druhý balíček: ")
-	for i := 0; i < len((*secondPack)); i++{
+	for i := 0; i < len((*secondPack)); i++ {
 		printCard((*secondPack)[i])
 	}
 	fmt.Println()
@@ -86,25 +86,25 @@ func main() {
 	var pack []int
 	var packOne []int
 	var packTwo []int
-	if len(args) == 1{
+	if len(args) == 1 {
 		i, err := strconv.Atoi(args[0])
 		if err != nil {
 			panic(err)
 		}
 		packSize = i
 		pack = genPacks(packSize)
-		packOne = pack[0 : (packSize / 2) + 1]
-		packTwo = pack[(packSize / 2) - 1 : packSize]
-	} else if len(args) == 2{
-		for i := 0; i < len(args[0]); i++{
-			if args[0][i] == '1'{
+		packOne = pack[0 : (packSize/2)+1]
+		packTwo = pack[(packSize/2)-1 : packSize]
+	} else if len(args) == 2 {
+		for i := 0; i < len(args[0]); i++ {
+			if args[0][i] == '1' {
 				packOne = append(packOne, 1)
 			} else {
 				packOne = append(packOne, 0)
 			}
 		}
-		for i := 0; i < len(args[1]); i++{
-			if args[1][i] == '1'{
+		for i := 0; i < len(args[1]); i++ {
+			if args[1][i] == '1' {
 				packTwo = append(packTwo, 1)
 			} else {
 				packTwo = append(packTwo, 0)
@@ -112,23 +112,22 @@ func main() {
 		}
 	} else {
 		pack = genNormalPack()
-		packOne = pack[0 : (len(pack) / 2) + 1]
-		packTwo = pack[(len(pack) / 2) - 1 : len(pack)]
+		packOne = pack[0 : (len(pack)/2)+1]
+		packTwo = pack[(len(pack)/2)-1 : len(pack)]
 	}
 	round := 0
 	fmt.Print("Balíček: ")
-	for i := 0; i < packSize; i++{
+	for i := 0; i < packSize; i++ {
 		fmt.Print(pack[i])
 	}
 	fmt.Println()
 	printPacks(&packOne, &packTwo)
 	playingFirst := true
-	for len(packOne) != 0 && len(packTwo) != 0{
+	for len(packOne) != 0 && len(packTwo) != 0 {
 		playingFirst = singleStep(playingFirst, &packOne, &packTwo)
 		printPacks(&packOne, &packTwo)
-		round ++
+		round++
 	}
 	fmt.Println("Počet kol:")
 	fmt.Println(round)
 }
-
