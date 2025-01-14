e36a8ce95455271629eaa0182c5ee4e60a724171
Tomáš Turek <tom.turek@atlas.cz>
2025-01-14 14:42:27 +0100

    simple game with bears

---
+++ bears/bears.rb
@@ -0,0 +1,29 @@
+$dices = ["\u{2680}", "\u{2681}", "\u{2682}", "\u{2683}", "\u{2684}", "\u{2685}"]
+$bears = [0, 0, 2, 0, 4, 0]
+
+def get_dices()
+  ret = [0, 0, 0, 0]
+  (0..3).each do |i|
+	tmp = rand(0..5)
+	ret[i] = [$dices[tmp], $bears[tmp]]
+  end
+  return ret
+end
+
+loop do
+  values = get_dices()
+  puts "How many bears are near the lake?\n#{values[0][0]} #{values[1][0]}\n#{values[2][0]} #{values[3][0]}"
+  sum = 0
+  values.each do |val|
+	sum += val[1]
+  end
+  input = gets.chomp
+  break if input.downcase == "exit"
+  if sum == Integer(input)
+	puts "That is correct answer."
+  else
+	puts "No, correct answer is #{sum}."
+  end
+end
+
+puts "Terminated"
